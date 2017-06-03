06ce7d2290e5dc8a9aecf810389f0526fc4bea49
James Harris <dev@jharris.eu>
2017-06-03 21:51:54 +1000

    Refactored init into promise Added more concrete error handling Cleaned up the code Added comments

--- server.js
+++ server.js
@@ -4,33 +4,47 @@ const helmet 		= require('helmet');
 const sqlite3 		= require('sqlite3').verbose();
 const server 		= express();
 const cors          = require('./app/cors');
+const sourceFile    = './db/bsb.csv';
 
 var db              = new sqlite3.Database(':memory:');
-var createDatabase 	= require('./app/createDatabase')(db, './db/bsb.csv');
+var init 	        = require('./app/init');
 var bsbRouter 	    = require('./app/bsbRouter')(db);
 
-server.use(helmet());
-server.use(bodyParser.json());
-server.use(cors);
-server.use('/api/v1/bsb', bsbRouter);
+server.use(helmet());           // Secure the express app
+server.use(bodyParser.json());  // Parse JSON requests
+server.use(cors);               // Allow cross domain usage of the API
+server.use('/api/v1/bsb', bsbRouter); // Configure our API routes
 
-server.get('/', function(req, res, next) {
-	res.send("BSB API");
-});
-
+/**
+ * Global error handler
+ */
 server.use(function (err, req, res, next) {
-  console.error(err);
-  res.status(400).send(err);
-})
-
-server.listen(8080, function () {
-  	console.log('BSB service started on port 8080');
+    if(err.status) {
+        res.status(err.status).json({message: err.message});
+    } else {
+        console.error(err);
+        res.status(500).send('Server error');
+    }
 });
 
+/**
+ * Start the app
+ */
+init(db, sourceFile).then(function() {
+    server.listen(8080, function () {
+        console.log('BSB service started on port 8080');
+    });
+}, (err) => {
+    console.error('Failed to initialize:', err);
+});
+
+/**
+ * Cleanup on exit
+ */
 process.on('exit', function() {
     db.close();
 });
 
 process.on('SIGINT', function() {
-    process.exit(2);
+    process.exit(2); // Calls above exit function
 });
